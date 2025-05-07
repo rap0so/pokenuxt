3720791285727c8631e639c4eddbf28e4abf960f
Jeremy Graziani <jeremy.graziani@nextory.com>
2025-05-07 19:27:28 +0200

    🍍 Add Pinia for state management support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/test-utils',
     '@unocss/nuxt',
+    '@pinia/nuxt',
   ],
   devtools: { enabled: true },
   compatibilityDate: '2024-11-01',
