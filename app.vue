1f7d323af0aae403282cffbc5f607f60a0426166
Jeremy Graziani <jeremy.graziani@nextory.com>
2025-05-07 12:41:55 +0200

    ✨ Integrate UnoCSS for styling and add Tailwind CSS reset

--- app.vue
+++ app.vue
@@ -4,3 +4,20 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+import '@unocss/reset/tailwind.css' // Importing Tailwind CSS reset styles
+
+useHead({
+  bodyAttrs: {
+    class: 'bg-stone-100 m-0', // Setting the background color and margin for the body
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico', // Setting the favicon for the website
+    },
+  ],
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
     '@nuxt/test-utils',
+    '@unocss/nuxt',
   ],
   devtools: { enabled: true },
   compatibilityDate: '2024-11-01',
